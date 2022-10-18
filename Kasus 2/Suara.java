3993394df4421a8f8e3c37fae8cdf8e614fbaa04
rstubryan <96672455+rstubryan@users.noreply.github.com>
2022-10-18 10:43:00 +0700

    Add files via upload

---
+++ Kasus 2/Suara.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Kasus1;
+
+/**
+ *
+ * @author Restu
+ */
+class Bel {
+    public void suara(){
+        System.out.println("TEEEEEEEEEEEETTTTTTTTTTTT");
+    }
+}
+
+class Sepeda extends Bel {
+    @Override
+    public void suara(){
+        System.out.println("Kring kring");
+    }
+}
+
+public class Suara {
+    public static void main(String [] args){
+        Bel sepeda = new Sepeda();
+        sepeda.suara();
+    }
+}
