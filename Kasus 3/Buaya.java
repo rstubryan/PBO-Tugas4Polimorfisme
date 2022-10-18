d9a4a5fb2b83a314399a51cffc54db8e9977a1e3
rstubryan <96672455+rstubryan@users.noreply.github.com>
2022-10-18 10:27:32 +0700

    Add files via upload

---
+++ Kasus 3/Buaya.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Kasus2;
+
+/**
+ *
+ * @author Restu
+ */
+public class Buaya extends Hewan {
+
+    //overriding
+    @Override
+    public void makan() {
+        System.out.println("Buaya makan daging");
+    }
+    
+    //overloading
+	public void makan(String s) {
+            System.out.println("Buaya makan " + s);
+	}
+
+	public static void main(String[] args) {
+		Buaya satu = new Buaya();
+		satu.makan();
+
+		Buaya dua = new Buaya();
+		dua.makan("manusia");
+
+		Buaya tiga = new Buaya();
+		tiga.makan("omongan sendiri");
+
+		Buaya empat = new Buaya();
+		empat.makan("temen");
+
+		Buaya lima = new Buaya();
+	}
+}
